47b861f377c627eedbb8926c88853a677fd00b6b
Jose <jose.s.figueroa6@gmail.com>
2016-02-06 09:56:14 -0800

    Implemented a DBUser class which uses the User interface. This class stores the username, email, and password of a User. THe TestDBUser class is a JUnit test for the DBUser class.

---
+++ src/main/java/edu/cpp/cs580/Database/Objects/DBUser.java
@@ -0,0 +1,64 @@
+package edu.cpp.cs580.Database.Objects;
+
+public class DBUser implements User {
+	/******************Data Members********************/
+	private String email,
+				   password,
+				   username;
+	
+	/******************Constructors*******************/
+	/**
+	 * Creates a default user with nothing initialized.
+	 */
+	public DBUser() {
+		email = null;
+		password = null;
+		username = null;
+	}
+	
+	/**
+	 * Creates a DBUser with the username, email, and password initalized
+	 * to the given parameters.
+	 * 
+	 * @param uname	User username
+	 * @param mail	User email
+	 * @param pass	User password
+	 */
+	public DBUser(String uname, String mail, String pass) {
+		username = uname;
+		email = mail;
+		password = pass;
+	}
+	
+	/******************Methods************************/
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public void setEmail(String em) {
+		email = em;
+	}
+
+	@Override
+	public void setPassword(String ps) {
+		password = ps;
+	}
+	
+	@Override
+	public void setUsername(String un) {
+		username = un;
+	}
+
+}
---
+++ src/test/java/edu/cpp/cs580/Database/TestDBUser.java
@@ -0,0 +1,42 @@
+package edu.cpp.cs580.Database;
+
+import org.junit.Test;
+
+import edu.cpp.cs580.Database.Objects.DBUser;
+import edu.cpp.cs580.Database.Objects.User;
+import org.junit.Assert;
+
+public class TestDBUser {
+	@Test
+	public void testDBUserInitalizedConstructor() {
+		String u = "Test",
+			   e = "test@test",
+			   p = "password";
+		//Test initialized constructor and get methods
+		User user = new DBUser(u, e, p);
+		Assert.assertEquals(u, user.getUsername());
+		Assert.assertEquals(e, user.getEmail());
+		Assert.assertEquals(p, user.getPassword());
+	}
+	
+	@Test
+	public void testDBUserDefaultConstructor() {
+		String u = "Test2",
+			   e = "test2@test",
+			   p = "password2";
+		
+		//Test default constructors
+		User user = new DBUser();
+		Assert.assertNull(user.getUsername());
+		Assert.assertNull(user.getEmail());
+		Assert.assertNull(user.getPassword());
+		
+		//Test set methods
+		user.setEmail(e);
+		user.setPassword(p);
+		user.setUsername(u);
+		Assert.assertEquals(u, user.getUsername());
+		Assert.assertEquals(e, user.getEmail());
+		Assert.assertEquals(p, user.getPassword());
+	}
+}
